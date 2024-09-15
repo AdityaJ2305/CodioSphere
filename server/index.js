b0ed8c073c44ff24450b08e1e9674e09ec8c76c4
AdityaJ2305 <adityajindal2305@gmail.com>
2024-09-15 11:45:22 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import { Server as SocketIoServer } from 'socket.io';
+import http from 'http';
+
 const app = express();
+const server = http.createServer(app);
+const io = new SocketIoServer(server);
 
 app.get('/',(req,res)=>{
-    return res.send("<div>Hello how are you </div>");
+    return res.send("<div>Hello how are you ? </div>");
 })
+io.on('connection', (socket) => {
+    console.log('socket connected ', socket.id);
+});
 
-app.listen(4004,()=>{
-    console.log('server listening');
-})
+const PORT = process.env.PORT || 5009;
+
+server.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
