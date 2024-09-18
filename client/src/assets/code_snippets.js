e286628ff3c12024b76c0b6ce8d809bebcf64fb3
AdityaJ2305 <adityajindal2305@gmail.com>
2024-09-18 19:48:51 +0530

    init

--- client/src/assets/code_snippets.js
+++ client/src/assets/code_snippets.js
@@ -1,14 +1,14 @@
 const codeSnippets = {
-    "c": `#include <stdio.h>\n\nvoid greet() {\n\tprintf("Hello From CodioSphere\\n");\n}\n\nint main() {\n\tgreet();\n\treturn 0;\n}`,
+    "c": `#include <stdio.h>\n\nvoid greet() {\n\tprintf("Hello from CodioSphere\\n");\n}\n\nint main() {\n\tgreet();\n\treturn 0;\n}`,
     
-    "cpp": `#include <iostream>\n\nint main() {\n\tstd::cout << "Hello From CodioSphere";\n\t return 0;
+    "cpp": `#include <iostream>\n\nint main() {\n\tstd::cout << "Hello from CodioSphere";\n\t return 0;
 }`,
     
-    "java": `public class Main {\n\tpublic static void greet() {\n\t\tSystem.out.println("Hello From CodioSphere");\n\t}\n\n\tpublic static void main(String[] args) {\n\t\tgreet();\n\t}\n}`,
+    "java": `public class Main {\n\tpublic static void greet() {\n\t\tSystem.out.println("Hello from CodioSphere");\n\t}\n\n\tpublic static void main(String[] args) {\n\t\tgreet();\n\t}\n}`,
     
-    "python": `def greet():\n\tprint("Hello From CodioSphere")\n\ngreet()\n`,
+    "python": `def greet():\n\tprint("Hello from CodioSphere")\n\ngreet()\n`,
     
-    "javascript": `function greet() {\n\tconsole.log("Hello From CodioSphere");\n}\ngreet();\n`,
+    "javascript": `function greet() {\n\tconsole.log("Hello from CodioSphere");\n}\ngreet();\n`,
 
     "plaintext": `Hi, CodioSphere Here`
 };
