054b476eb33eabcea398a739d52c6e794622473f
AdityaJ2305 <adityajindal2305@gmail.com>
2024-09-25 00:27:17 +0530

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 import SignIn from "./components/Signinpage/Signin";
-// import { SpeedInsights } from "@vercel/speed-insights/react"
 import EditorPage from "./components/EditorPage/EditorPage";
 import { Route, Routes } from "react-router-dom";
 
