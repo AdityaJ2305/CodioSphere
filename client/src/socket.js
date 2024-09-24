11416f71a376f3582e0f03a44b5256d76d8566b7
AdityaJ2305 <adityajindal2305@gmail.com>
2024-09-25 00:27:37 +0530

    socket client instance

--- client/src/socket.js
+++ client/src/socket.js
@@ -7,7 +7,7 @@ export const initSocket = async ()=>{
         timeout: 1000,
         transport:['websocket']
     }
-    return io(process.env.REACT_APP_BACKEND_URL, options);
+    return io("http://localhost:5006", options);
     // it will return socket client instance
     // it is the url on which our server is running
 }
