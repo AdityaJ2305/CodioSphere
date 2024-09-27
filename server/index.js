74afb2f2c76c77ad800343ed6f297e4ab8e23d29
AdityaJ2305 <adityajindal2305@gmail.com>
2024-09-27 14:19:21 +0530

    index file added

--- server/index.js
+++ server/index.js
@@ -1,20 +1,48 @@
 import express from 'express';
-import { Server as SocketIoServer } from 'socket.io';
 import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketIoServer(server);
-
-app.get('/',(req,res)=>{
-    return res.send("<div>Hello how are you ? </div>");
-})
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3009",
+        credentials: true 
+    }
+});
+const getAllClient = (roomid)=>{
+    return Array.from(io.sockets.adapter.rooms.get(roomid) || []).map((socketId)=>{
+        return {
+            socketId,
+            username: userMap[socketId]
+        }
+    })
+}
+const userMap = {};
+// Socket.io connection handler
 io.on('connection', (socket) => {
-    console.log('socket connected ', socket.id);
+    console.log(`a user with socket id ${socket.id}`);
+
+    // Handle events, e.g. joining a room
+    socket.on('join', ({roomId}) => {
+        userMap[socket.id] = "tinka";
+        socket.join(roomId);
+        const clients = getAllClient(roomId);
+        console.log(clients);
+        clients.forEach((data)=>{
+            io.to(data.socketId).emit('joined',{
+                clients: clients,
+                socketId: data.socketId
+            })
+        })
+    });
+
+    socket.on('disconnected', () => {
+        console.log('user disconnected');
+    });
 });
 
 const PORT = process.env.PORT || 5006;
-
 server.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`Server listening on *:${PORT}`);
 });
